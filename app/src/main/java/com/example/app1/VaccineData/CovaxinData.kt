d90d63084354ed0b1ddcaa6395ccbff5ba307e1d
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2021-10-30 22:46:18 +0530

    Adding yt video player

--- app/src/main/java/com/example/app1/VaccineData/CovaxinData.kt
+++ app/src/main/java/com/example/app1/VaccineData/CovaxinData.kt
@@ -4,11 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import com.example.app1.databinding.FragmentCovaxinDataBinding
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+
+import androidx.annotation.NonNull
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+
+import android.R
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
 
 
 class CovaxinData : Fragment() {
+
 
     private lateinit var binding: FragmentCovaxinDataBinding
 
@@ -17,9 +30,21 @@ class CovaxinData : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentCovaxinDataBinding.inflate(inflater, container, false)
+        val youTubePlayerView: YouTubePlayerView = binding.youtubePlayerView
+        lifecycle.addObserver(youTubePlayerView)
+
+        youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                val videoId = "FtX0mLCqKXU"
+                youTubePlayer.loadVideo(videoId, 0f)
+            }
+        })
+
         val root: View = binding.root
-        return root    }
+        return root
+
+
+    }
 
 }
